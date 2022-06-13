20e3c205b95d266f9e5841f73f448acbf1968a42
elaina <gea.christensen@gmail.com>
2022-06-13 16:59:36 -0400

    add/remove functinoality to cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 80,
+    title: 'tamagotchi',
+    description: 'a very cute friend!',
+  },
+  {
+    id: 'p2',
+    price: 20,
+    title: 'book',
+    description: 'a very interesting book',
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
